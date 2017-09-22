ccd7dcdaf1f8f6f27df373dbce6dc6e168c5116c
Hlidskjalf <vroman197@gmail.com>
2017-09-22 13:21:47 -0400

    Initial commit

---
+++ src/Week_one.java
@@ -0,0 +1,119 @@
+import java.util.Scanner;
+
+public class Week_one {
+
+    public static void main(String[] args) {
+        //name();
+        //HelloWorld();
+        //Spruce();
+        //farmTracker();
+        //secondsPerYear();
+        //add();
+        //multiply();
+        //adder();
+        //circumference();
+        findTheBiggerNumber();
+    }
+
+    public static void name() {
+        System.out.println("Andrew Vroman");
+    }
+
+    public static void HelloWorld() {
+        System.out.println("Hello World!");
+        System.out.println("(And all the people of the world)");
+    }
+
+    public static void Spruce() {
+        System.out.println("    *    ");
+        System.out.println("   ***   ");
+        System.out.println("  *****  ");
+        System.out.println(" ******* ");
+        System.out.println("*********");
+        System.out.println("    *    ");
+
+    }
+
+    public static void farmTracker() {
+        int chickens = 3;
+        double baconWeight = 5.5;
+        String tractor = "There is none!";
+
+        System.out.println("Chickens: \n" + chickens + "\nBacon (kg): \n" + baconWeight + "\nTractor: \n" + tractor + "\n");
+        System.out.println("In a nutshell: \n" + chickens + "\n" + baconWeight + "\n" + tractor + "\n");
+
+        System.out.println("Let's modify these numbers programmatically.\n");
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("How many Chickens are there?");
+        chickens = input.nextInt();
+        System.out.println("How much bacon is there? (kg)");
+        baconWeight = input.nextDouble();
+        System.out.println("What model of tractor is there?");
+        tractor = input.next();
+
+        System.out.println("Chickens: \n" + chickens + "\nBacon (kg): \n" + baconWeight + "\nTractor: \n" + tractor + "\n");
+        System.out.println("In a nutshell: \n" + chickens + "\n" + baconWeight + "\n" + tractor + "\n");
+
+    }
+
+    public static void secondsPerYear() {
+        double seconds;
+        seconds = (60 * 60 * 24 * 365);
+
+        System.out.println("There are " + seconds + " seconds per year.");
+    }
+
+    public static void add() {
+        int x = 5;
+        int y = 4;
+
+        System.out.println(x + " + " + y + " = " + (x + y));
+    }
+
+    public static void multiply() {
+        int x = 7;
+        int y = 10;
+
+        System.out.println(x + " * " + y + " = " + (x * y));
+    }
+
+    public static void adder() {
+        int x;
+        int y;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        x = input.nextInt();
+        System.out.println("Enter another number: ");
+        y = input.nextInt();
+
+        System.out.println("Sum of the numbers: " + (x + y));
+    }
+
+    public static void circumference() {
+        double pi = Math.PI;
+        double radius;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the radius of a circle: ");
+        radius = input.nextDouble();
+
+        System.out.println("The circumference is " + ((2 * pi) * radius));
+
+    }
+
+    public static void findTheBiggerNumber() {
+        int x;
+        int y;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        x = input.nextInt();
+        System.out.println("Enter another number: ");
+        y = input.nextInt();
+        int larger = (x > y) ? x : y;
+        System.out.println("The larger of the two is: " + larger);
+    }
+}
